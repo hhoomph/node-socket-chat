b6bebba5b1b9ff4a592aab3e19296fa8a300bf18
hhoomph <hh.oomph@gmail.com>
2021-06-23 14:59:12 +0430

    socket problem

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const userCollection = "onlineUsers"; //collection to maintain list of currently
 const port = process.env.PORT || 3000;
 const database = "mongodb://localhost:27017/";
 const app = express();
-// const server = http.createServer(app);
-const server = app.listen(process.env.PORT || 3000);
+const server = http.createServer(app);
+// const server = app.listen(process.env.PORT || 3000);
 const io = socketio(server);
 // server css as static
 app.use(express.static(__dirname));
@@ -222,6 +222,7 @@ io.on("connection", (socket) => {
       .catch((err) => console.error("Error: %s", err));
   });
 });
-server.listen(port, () => {
-  console.log(`Chat Server listening to port ${port}...`);
-});
+// server.listen(port, () => {
+//   console.log(`Chat Server listening to port ${port}...`);
+// });
+server.listen(port);
